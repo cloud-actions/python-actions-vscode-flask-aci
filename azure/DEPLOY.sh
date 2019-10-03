e3cf1e1a51b487d1f445b1d2122b6ef801f02700
Aaron Wislang <aaron.wislang@microsoft.com>
2019-10-03 15:10:34 -0400

    Add azure/* and 4-action-acr-aci.yml

---
+++ azure/DEPLOY.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# variables
+RESOURCE_GROUP='191000-python-actions-aks'
+LOCATION='eastus'
+RANDOM_STR='82ffdh'
+
+# clone app
+git clone https://github.com/microsoft/python-sample-vscode-flask-tutorial.git
+cd python-sample-vscode-flask-tutorial/
+# create container registry
+REGISTRY_NAME="acr${RANDOM_STR}"
+az acr create -g $RESOURCE_GROUP -l $LOCATION --name $REGISTRY_NAME --sku Basic --admin-enabled true
+# build image
+CONTAINER_IMAGE=hello-python-flask:$(date +%y%m%d)-${{ github.sha }}
+az acr build -r $REGISTRY_NAME -t $CONTAINER_IMAGE --file Dockerfile .
+# create container instance
+REGISTRY_PASSWORD=$(az acr credential show -n $REGISTRY_NAME | jq -r .passwords[0].value)
+CONTAINER_NAME="aci${RANDOM_STR}"
+az container create --resource-group $RESOURCE_GROUP --location $LOCATION \
+    --name $CONTAINER_NAME \
+    --image "${REGISTRY_NAME}.azurecr.io/${CONTAINER_IMAGE}" \
+    --registry-login-server "${REGISTRY_NAME}.azurecr.io" \
+    --registry-username $REGISTRY_NAME \
+    --registry-password $REGISTRY_PASSWORD \
+    --cpu 1 \
+    --memory 1 \
+    --ports 80 \
+    --environment-variables LISTEN_PORT=80 \
+    --dns-name-label $CONTAINER_NAME
+FQDN=$(az container show -g $RESOURCE_GROUP --name $CONTAINER_NAME | jq -r .ipAddress.fqdn)
+echo "http://${FQDN}"
