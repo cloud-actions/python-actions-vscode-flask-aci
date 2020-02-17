6c936641db595403b5ab0e29d4a9fc1d86d6d007
Aaron Wislang <aaron.wislang@microsoft.com>
2020-02-16 22:26:36 -0500

    Update to vscode-flask

--- azure/DEPLOY.sh
+++ azure/DEPLOY.sh
@@ -6,7 +6,7 @@ RESOURCE_GROUP='200200-actions'
 LOCATION='eastus'
 RANDOM_STR='09c205'
 # [[ -z "$RANDOM_STR" ]] && RANDOM_STR=$(openssl rand -hex 3)
-REPOSITORY_NAME="hello-python"
+REPOSITORY_NAME="hello-vscode-flask"
 
 # create container registry
 REGISTRY_NAME="acr${RANDOM_STR}"
