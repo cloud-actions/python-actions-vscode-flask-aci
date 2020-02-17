53adce083f485f78f3ca5f66913d40ac8b0d7318
Aaron Wislang <aaron.wislang@microsoft.com>
2020-02-16 22:09:05 -0500

    Add REPOSITORY_NAME

--- azure/DEPLOY.sh
+++ azure/DEPLOY.sh
@@ -6,16 +6,17 @@ RESOURCE_GROUP='200200-actions'
 LOCATION='eastus'
 RANDOM_STR='09c205'
 # [[ -z "$RANDOM_STR" ]] && RANDOM_STR=$(openssl rand -hex 3)
+REPOSITORY_NAME="hello-python"
 
 # create container registry
 REGISTRY_NAME="acr${RANDOM_STR}"
 az acr create -g $RESOURCE_GROUP -l $LOCATION --name $REGISTRY_NAME --sku Basic --admin-enabled true
 # build image
-CONTAINER_IMAGE=hello-python-flask:$(date +%y%m%d)-${GITHUB_SHA}
+CONTAINER_IMAGE=$REPOSITORY_NAME:$(date +%y%m%d)-${GITHUB_SHA}
 az acr build -r $REGISTRY_NAME -t $CONTAINER_IMAGE --file Dockerfile .
 # create container instance
 REGISTRY_PASSWORD=$(az acr credential show -n $REGISTRY_NAME | jq -r .passwords[0].value)
-CONTAINER_NAME="aci${RANDOM_STR}"
+CONTAINER_NAME="aci-${REPOSITORY_NAME}-${RANDOM_STR}"
 az container create --resource-group $RESOURCE_GROUP --location $LOCATION \
     --name $CONTAINER_NAME \
     --image "${REGISTRY_NAME}.azurecr.io/${CONTAINER_IMAGE}" \
@@ -26,6 +27,6 @@ az container create --resource-group $RESOURCE_GROUP --location $LOCATION \
     --memory 1 \
     --ports 80 \
     --environment-variables LISTEN_PORT=80 \
-    --dns-name-label $CONTAINER_NAME
+    --dns-name-label ${REPOSITORY_NAME}-${RANDOM_STR}
 FQDN=$(az container show -g $RESOURCE_GROUP --name $CONTAINER_NAME | jq -r .ipAddress.fqdn)
 echo "http://${FQDN}"
